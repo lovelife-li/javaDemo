7f2accf0ef21bcad90ffdc636997b4216d4d1080
liumin <liumin@cmos.chinamobile.com>
2020-03-18 20:38:30 +0800

    test

--- javatest/src/main/java/com/study/thread/RateLimiterTest.java
+++ javatest/src/main/java/com/study/thread/RateLimiterTest.java
@@ -3,9 +3,14 @@ package com.study.thread;
 import com.google.common.util.concurrent.RateLimiter;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.Long.max;
+import static java.lang.Math.min;
 
 /**
  * @author ldb
@@ -39,4 +44,113 @@ public class RateLimiterTest {
         System.out.println("hello");
     }
 
+    @Test
+    public void test2(){
+        long l = System.nanoTime();
+        Date date = new Date(l / 1_000_000);
+        System.out.println(l);
+        System.out.println(date);
+
+        long r = System.currentTimeMillis();
+        Date date2 = new Date(r);
+        System.out.println(r);
+        System.out.println(date2);
+    }
+
+}
+class SimpleLimiter {
+    // 下一令牌产生时间
+    long next = System.nanoTime();
+    // 发放令牌间隔：纳秒
+    long interval = 1000_000_000;
+    // 预占令牌，返回能够获取令牌的时间
+    synchronized long reserve(long now){
+        // 请求时间在下一令牌产生时间之后
+        // 重新计算下一令牌产生时间
+        if (now > next){
+            // 将下一令牌产生时间重置为当前时间
+            next = now;
+        }
+        // 能够获取令牌的时间
+        long at=next;
+        // 设置下一令牌产生时间
+        next += interval;
+        // 返回线程需要等待的时间
+        return Math.max(at, 0L);
+    }
+    // 申请令牌
+    void acquire() {
+        // 申请令牌时的时间
+        long now = System.nanoTime();
+        // 预占令牌
+        long at=reserve(now);
+        long waitTime=max(at-now, 0);
+        // 按照条件等待
+        if(waitTime > 0) {
+            try {
+                TimeUnit.NANOSECONDS
+                        .sleep(waitTime);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class SimpleLimiter2 {
+        // 当前令牌桶中的令牌数量
+        long storedPermits = 0;
+        // 令牌桶的容量
+        long maxPermits = 3;
+        // 下一令牌产生时间
+        long next = System.nanoTime();
+        // 发放令牌间隔：纳秒
+        long interval = 1000_000_000;
+
+        // 请求时间在下一令牌产生时间之后, 则
+        // 1. 重新计算令牌桶中的令牌数
+        // 2. 将下一个令牌发放时间重置为当前时间
+        void resync(long now) {
+            if (now > next) {
+                // 新产生的令牌数
+                long newPermits=(now-next)/interval;
+                // 新令牌增加到令牌桶
+                storedPermits=min(maxPermits,
+                        storedPermits + newPermits);
+                // 将下一个令牌发放时间重置为当前时间
+                next = now;
+            }
+        }
+        // 预占令牌，返回能够获取令牌的时间
+        synchronized long reserve(long now){
+            resync(now);
+            // 能够获取令牌的时间
+            long at = next;
+            // 令牌桶中能提供的令牌
+            long fb=min(1, storedPermits);
+            // 令牌净需求：首先减掉令牌桶中的令牌
+            long nr = 1 - fb;
+            // 重新计算下一令牌产生时间
+            next = next + nr*interval;
+            // 重新计算令牌桶中的令牌
+            this.storedPermits -= fb;
+            return at;
+        }
+        // 申请令牌
+        void acquire() {
+            // 申请令牌时的时间
+            long now = System.nanoTime();
+            // 预占令牌
+            long at=reserve(now);
+            long waitTime=max(at-now, 0);
+            // 按照条件等待
+            if(waitTime > 0) {
+                try {
+                    TimeUnit.NANOSECONDS
+                            .sleep(waitTime);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
